99998f125903367fccb5c6600e531123d1d48243
togashigreat <muhammadmokit2006@gmail.com>
2024-12-05 18:15:22 +0600

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'AnimeScraper'
 copyright = '2024, Muhammad MuQiT'
 author = 'Muhammad MuQiT'
-release = "1.0.4"
+release = "1.0.5"
 version = release
 
 # -- General configuration ---------------------------------------------------
--- t.py
+++
@@ -1,8 +0,0 @@
-from AnimeScraper import KunYu
-import asyncio
-async def m():
-    s = KunYu()
-    anime = await s.search_anime("Clannad")
-    print(anime.stats)
-
-asyncio.run(m())
